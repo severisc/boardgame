c97389d2944e567223e461c1cd49aa6215a2792d
Sever Ilie <severisc@gmail.com>
2019-04-15 19:44:22 +0300

    Initial commit

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+]
