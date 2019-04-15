5dba862e60494ec13014e3cd82ad79ef992350d2
Sever Ilie <severisc@gmail.com>
2019-04-15 20:15:31 +0300

    adaugat si parametru pentru main home.html

--- main/views.py
+++ main/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 #    return HttpResponse("Hello, World")
 
 def home(request):
-    return render(request, "main/home.html")
+    return render(request,  "main/home.html",{'message':'Hi, there__!'})
