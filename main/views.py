8579aee8695d813f9d4d493757294a64a2c05c19
Sever Ilie <severisc@gmail.com>
2019-04-15 20:09:16 +0300

    add template main home.html

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#def home(request):
+#    return HttpResponse("Hello, World")
+
 def home(request):
-    return HttpResponse("Hello, World")
+    return render(request, "main/home.html")
